c683baf1c7da88c86cd124e9fc2632229a91f309
Ar G Ho <41030992+iArgho@users.noreply.github.com>
2025-07-24 18:34:29 +0600

    24

--- lib/view/home/home/home_screen.dart
+++ lib/view/home/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:home_cache/constants/colors.dart';
+import 'package:home_cache/constants/text_style.dart';
+import 'package:home_cache/view/widget/app_bar_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,16 +10,79 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBarWidget(),
       backgroundColor: AppColors.surface,
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(24.sp),
-            child: Text(
-              "Home Screen",
-              style: TextStyle(fontSize: 22.sp, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.lightgrey.withOpacity(.7),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 10.h),
+                  Image.asset("assets/images/dash.png", height: 240.h),
+                  Container(
+                    padding: EdgeInsets.only(left: 24.w, right: 24.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Great Work!',
+                          style: TextStyles.medium.copyWith(
+                            color: AppColors.black,
+                            fontSize: 24.sp,
+                          ),
+                        ),
+                        Text(
+                          'You’ve completed 4/5 tasks this Season!',
+                          style: TextStyles.medium.copyWith(
+                            color: AppColors.secondary,
+                            fontSize: 18.sp,
+                          ),
+                        ),
+                        SizedBox(height: 10.h),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                ],
+              ),
             ),
-          ),
+
+            SizedBox(height: 10.h),
+            Container(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Tasks',
+                    style: TextStyles.medium.copyWith(
+                      color: AppColors.black,
+                      fontSize: 18.sp,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
